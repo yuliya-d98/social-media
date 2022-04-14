37e135d4495f895de44f3592fc6c35f356283038
Yuliya Dubovik <feelgloom@gmail.com>
2022-04-14 22:28:53 +0300

    feat: open user by userId in url

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ function App(props) {
         <Sidebar />
         <div className="content-container">
           <Routes>
-            <Route path="/profile/*" element={<ProfileContainer />} />
+            <Route path="/profile" element={<ProfileContainer />}>
+              <Route path=":userId" element={<ProfileContainer />} />
+            </Route>
             <Route path="/dialogs/*" element={<DialogsContainer />} />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
--- src/components/main/profile/profileContainer.jsx
+++ src/components/main/profile/profileContainer.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Profile from './profile';
 import { setUserProfile } from '../../../redux/profile-reducer'
+import { useMatch } from 'react-router-dom';
 
 class ProfileContainer extends React.Component {
     componentDidMount() {
+        const userId = this.props.match ? this.props.match.params.userId : 2;
         axios
-            .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+            .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
             .then(responce => {
                 this.props.setUserProfile(responce.data);
             })
@@ -25,4 +27,14 @@ const mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
 })
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+export const withRouter = (Component) => {
+    const RouterComponent = (props) => {
+        const match = useMatch('/profile/:userId/');
+        return <Component {...props} match={match} />;
+    }
+    return RouterComponent;
+}
+
+const WithUrlDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, { setUserProfile })(WithUrlDataContainerComponent);
